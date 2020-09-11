acbbed83892324ae04a77bf9d2868de6ef76cb3f
AdriGeorge <49452066+AdriGeorge@users.noreply.github.com>
2020-09-11 13:57:27 +0200

    Update startnode.sh

--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-geth --allow-insecure-unlock --datadir node2/ --syncmode 'full' --port 30312 --rpc --rpcaddr 'localhost' --rpcport 8502 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes 'enode://c3cc53a5730793649f43c985cfd154de4c71607d4f003d02c55104d0ac444e8c4ea793ce2fb3d99a43b00c8b98f3b6caf2e2aecba2a95768d9b00646e1e5624d@192.168.178.23:0?discport=30310' --networkid 1515 --gasprice '0' --unlock '0x31A58DAd6C4128Dd5cEA3b0F40d1a23cD0f7265b' --password node2/password.txt --mine --ipcpath "~/blkSett/node2/geth.ipc"
+geth --datadir node/ --syncmode 'full' --port 30313 --rpc --rpcaddr 'localhost' --rpcport 8503 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes 'enode://c3cc53a5730793649f43c985cfd154de4c71607d4f003d02c55104d0ac444e8c4ea793ce2fb3d99a43b00c8b98f3b6caf2e2aecba2a95768d9b00646e1e5624d@192.168.178.23:0?discport=30310' --networkid 1515 --gasprice '0' --mine --ipcpath "~/EthereumPrivateNode/node/geth.ipc"
